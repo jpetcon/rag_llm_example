fd2a536b61b0449d1700d033b8403abff3670e7a
jamiesession <jamie@session.id>
2024-10-11 11:29:12 +0100

    added more  terraform

--- infrastructure/setup/main.tf
+++ infrastructure/setup/main.tf
@@ -1,1 +1,37 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+
+module "s3-bucket-generation"{
+    source = "../modules/s3"
+}
+
+
+module "lambda-query-generation"{
+    source = "../modules/lambda"
+
+    ecr_image_uri = ""
+}
+
+module "ecs-vector-generation-pipeline"{
+    source = "../modules/ecs"
+
+    aws_account_id = ""
+    ecr_image_uri = ""
+    subnets = ""
+    security_groups = ""
+    api_secret_name_pinecone = ""
+
+}
